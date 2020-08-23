7b75162188ef64a83311bbe4c23157ad0802ab8b
Catalin Plesu <catalinhimself@gmail.com>
2020-08-23 16:03:32 +0300

    balanced colors replaced page mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,5 @@ var themechanger = document.getElementById("theme");
 var body = document.getElementsByTagName("body")[0];
 themechanger.addEventListener("click" , () => {
     body.classList.toggle("color-change-dark");
-    bodyt.classList.toggle("color-change-light");
+    body.classList.toggle("color-change-light");
 })
